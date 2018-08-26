462029d531d656c8ff7d2f990655519137a197ae
Ein04 <reyget42@gmail.com>
2018-08-26 21:30:31 +0800

    css bug fixed

--- resources/views/static_pages/about.blade.php
+++ resources/views/static_pages/about.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.default')
 @section('title', 'ICCr - About')
 @section('content')
-	<h1> 关于ICCr </h1>
+	<h1> 关于拉莱耶 </h1>
+	用户体验极差的独立游戏评测及发布网站。
+
+	<h1> 关于ICCr</h1>
+	秉承独立、协作、创造的宗旨，用脚写游戏，实际上不存在的独立开发者。
 @stop
--- resources/views/static_pages/help.blade.php
+++ resources/views/static_pages/help.blade.php
@@ -2,4 +2,6 @@
 @section('title','ICCr - Help')
 @section('content')
 	<h1>帮助手册</h1>
+	你面向stackoverflow编程，你还想要什么帮助？
+	东西做得这么烂……随时要有推翻重来的决心。
 @stop
